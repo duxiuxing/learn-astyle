2c3660c6b1d1ab0317427f755173409de97a9185
duxiuxing <duxiuxing@foxmail.com>
2018-11-13 16:24:45 +0800

    更新simple astyle参数的推导用例

--- source/simple/05_pad_options_expect.cpp
+++ source/simple/05_pad_options_expect.cpp
@@ -1,37 +1,85 @@
-﻿// --break-blocks 不使用
+﻿/*
+    本文件的样本代码来自于astyle.html中的Padding Options部分
 
-// --pad-oper 操作符前后加空格
+    1. 不使用--break-blocks，避免引入多余的空行；
+    2. 使用--pad-oper，在操作符前后加空格；
+    3. 根据文档，使用了--pad-oper就不需要使用--pad-comma了；
+    4. 小括号内外不需要自动加空格，因此：
+    4.1 不使用--pad-paren；
+    4.2 不使用--pad-paren-out；
+    4.3 不使用--pad-first-paren-out；
+    4.4 不使用--pad-paren-in。
+    5. 使用--pad-header，if、for和while后面自动加空格；
+    6. 使用--unpad-paren，删除括号内多余的空格；
+    7. 不使用--delete-empty-lines，避免破坏代码的段落关系；
+    8. 不使用--fill-empty-lines，没有必要这么做；
+    9. 使用--align-pointer=type，指针声明中的*靠近类型；
+    10.使用--align-reference=type，引用声明中的&靠近类型；
+*/
+
+// 1. --break-blocks的代码片段
+isFoo = true;
+if (isFoo) {
+    bar();
+} else {
+    anotherBar();
+}
+isBar = false;
+
+// 2. --pad-oper的代码片段
 if (foo == 2) {
     a = bar((b - c) * a, d--);
 }
 
-// --pad-comma 不使用
+// 3. --pad-comma的代码片段
 if (isFoo(a, b)) {
     bar(a, b);
 }
 
-// --pad-paren 括号内外加空格，不使用
-// --pad-paren-out 仅括号外加空格，不使用
-// --pad-first-paren-out 仅第一层括号外加空格，不使用
-// --pad-paren-in 仅括号内加空格，不使用
-
-// --pad-header if, for, while后面加空格
+// 4.1 --pad-paren的代码片段
 if (isFoo((a + 2), b)) {
     bar(a, b);
 }
 
-// --unpad-paren 删除括号内多余的空格
+// 4.2 --pad-paren-out的代码片段
 if (isFoo((a + 2), b)) {
     bar(a, b);
 }
 
-// --delete-empty-lines 不使用
+// 4.3 --pad-first-paren-out的代码片段
+if (isFoo((a + 2), b)) {
+    bar(a, b);
+}
 
-// --align-pointer=name
-// --align-reference=name
-char* foo1;
-char& foo2;
+// 4.4 --pad-paren-in的代码片段
+if (isFoo((a + 2), b)) {
+    bar(a, b);
+}
 
+// 5. --pad-header的代码片段
+if (isFoo((a + 2), b)) {
+    bar(a, b);
+}
+
+// 6. --unpad-paren的代码片段
+if (isFoo((a + 2), b)) {
+    bar(a, b);
+}
+
+// 7. --delete-empty-lines的代码片段
+void Foo() {
+
+    foo1 = 1;
+
+    foo2 = 2;
+
+}
+
+// 9. --align-pointer=type的代码片段
 int* foo() {
-    bar(a, b);
+    char* foo1 = NULL;
+    return foo1;
 }
+
+// 10.--align-reference=type的代码片段
+char& foo2;
